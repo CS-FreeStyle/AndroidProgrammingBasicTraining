b2fe71a62d608d585546d6535ef2c29d41618c63
XINCGer <1790617178@qq.com>
2016-03-28 19:00:04 +0800

    隐式意图-打开系统照相机

---
+++ ActivityBasic/打开系统照相机/src/com/example/openCamera/Activity02.java
@@ -0,0 +1,12 @@
+package com.example.openCamera;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Activity02 extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity);
+	}
+}
---
+++ ActivityBasic/打开系统照相机/src/com/example/openCamera/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.openCamera;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		//获取界面上的按钮
+		Button button = (Button) findViewById(R.id.openCamera);
+		//给Button 按钮添加点击事件
+		button.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent();
+				intent.setAction("android.media.action.IMAGE_CAPTURE");
+				intent.addCategory("android.intent.category.DEFAULT");
+				startActivity(intent);
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
